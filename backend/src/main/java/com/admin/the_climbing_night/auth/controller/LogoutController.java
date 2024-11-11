18d6312fb3dafe33e05ee924532338ee70a956cb
김영선 <withbbang@gmail.com>
2024-11-11 13:59:09 +0900

    Feat: 로그아웃 기능 추가

--- backend/src/main/java/com/admin/the_climbing_night/auth/controller/LogoutController.java
+++ backend/src/main/java/com/admin/the_climbing_night/auth/controller/LogoutController.java
@@ -1,5 +1,32 @@
 package com.admin.the_climbing_night.auth.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.admin.the_climbing_night.common.SingleResponse;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping("/api")
 public class LogoutController {
-    
+    @PostMapping(value = "logout")
+    public SingleResponse logout(HttpServletResponse response) {
+        SingleResponse responseBody = new SingleResponse();
+
+        Cookie accessToken = new Cookie("accessToken", null);
+        Cookie refreshToken = new Cookie("refreshToken", null);
+
+        accessToken.setMaxAge(0);
+        refreshToken.setMaxAge(0);
+
+        response.addCookie(accessToken);
+        response.addCookie(refreshToken);
+
+        return responseBody;
+    }
 }
--- backend/src/main/java/com/admin/the_climbing_night/common/Constants.java
+++ backend/src/main/java/com/admin/the_climbing_night/common/Constants.java
@@ -1,5 +1,5 @@
 package com.admin.the_climbing_night.common;
 
 public final class Constants {
-    public static final String[] AUTH_WHITELIST = { "/", "/api/login", "/api/test", "/api/join" };
+    public static final String[] AUTH_WHITELIST = { "/", "/api/login", "/api/logout", "/api/test", "/api/join" };
 }
