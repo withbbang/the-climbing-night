389cff8d572abe6feecae3e757b2c09ab2d03b2b
김영선 <withbbang@gmail.com>
2024-11-16 23:42:53 +0900

    Refactor: 안 쓰는 import 제거

--- backend/src/main/java/com/admin/the_climbing_night/admin/domain/req/GetClimbingAreasRequest.java
+++ backend/src/main/java/com/admin/the_climbing_night/admin/domain/req/GetClimbingAreasRequest.java
@@ -1,7 +1,5 @@
 package com.admin.the_climbing_night.admin.domain.req;
 
-import com.admin.the_climbing_night.annotations.RequiredAnnotation;
-
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
