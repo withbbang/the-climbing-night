4d5882b30858f8e91804ce60b2952e24f4e0d814
김영선 <withbbang@gmail.com>
2024-11-20 20:28:12 +0900

    Style: Constants java 파일 주석 생성

--- backend/src/main/java/com/admin/the_climbing_night/common/Constants.java
+++ backend/src/main/java/com/admin/the_climbing_night/common/Constants.java
@@ -23,4 +23,9 @@ public final class Constants {
     public static String[] getAuthWhitelist() {
         return AUTH_WHITELIST;
     }
+
+    // 개발 환경마다 JWT 인가 확인 분기 필요 없을 때 사용
+    // public static final String[] AUTH_WHITELIST = new String[] { "/api/login",
+    // "/api/test", "/api/join",
+    // "/api/get-member-info-by-join" };
 }
